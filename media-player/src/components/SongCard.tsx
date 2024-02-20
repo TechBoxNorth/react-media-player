ef1c5eece6a3c2984130c630a4bec4d21a24627e
Anders <techboxnorth@gmail.com>
2024-02-20 21:42:12 +0100

    trying to render components from array of objects

--- media-player/src/components/SongCard.tsx
+++ media-player/src/components/SongCard.tsx
@@ -2,13 +2,18 @@
 import './SongCard.css/'
 
 function SongCard(){
+    
+    let imgPath:string = '';
+    let artist:string = '';
+    let song:string = '';
+    
     return (
         <>
             <div className="container hover">
-                <img src="" alt="" />
+                <img src={imgPath} alt="" />
                 <div className="song-info">
-                    <p className="artist">Artist Name</p>
-                    <p className="song">Song Name</p>
+                    <p className="artist">{artist}</p>
+                    <p className="song">{song}</p>
                 </div>
                 <div className="play-btn">
                     <span className="material-symbols-outlined md-48">play_circle</span>
--- media-player/src/components/SongsView.tsx
+++ media-player/src/components/SongsView.tsx
@@ -1,20 +1,61 @@
-import SongCard from './SongCard';
+import SongCard from './SongCard.tsx';
 import './SongsView.css'
 
 function SongsView(){
+    
+    interface ISong {
+        art: string;
+        artist: string;
+        song: string;
+    }
+
+    const songObjects:ISong[] = [
+        {
+            'art': './src/assets/being_boiled.jpg',
+            'artist': 'The Human League',
+            'song': 'Being Boiled'
+        },
+        {
+            'art': './src/assets/dancing_with_tears.jpg',
+            'artist': 'Ultravox',
+            'song': 'Dancing with tears in my eyes'
+        },
+        {
+            'art': './src/assets/fade_to_grey.jpg',
+            'artist': 'Visage',
+            'song': 'Fade to grey'
+        },
+        {
+            'art': './src/assets/maid_of_orleans.jpg',
+            'artist': 'Orchestral Manoeuvres in the Dark',
+            'song': 'Maid of Orlens'
+        },
+        {
+            'art': './src/assets/new_life.png',
+            'artist': 'Depeche Mode',
+            'song': 'New Life'
+        },
+        {
+            'art': './src/assets/only_you.jpg',
+            'artist': 'Yazoo',
+            'song': 'Only You'
+        },
+        {
+            'art': './src/assets/pocket_calculator.png',
+            'artist': 'Kraftwerk',
+            'song': 'Pocket Calculator'
+        }
+    ]
+    
+    
+    
+
     return (
         <>
             <div className="songs-container">
-                <SongCard />
-                <SongCard />
-                <SongCard />
-                <SongCard />
-                <SongCard />
-                <SongCard />
-                <SongCard />
-                <SongCard />
-                <SongCard />
-                <SongCard />
+                {
+                    songObjects.map(() => <SongCard />)
+                }
             </div>
         </>
     );
